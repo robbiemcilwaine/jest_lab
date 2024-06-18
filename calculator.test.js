f3a7aec0ffcab67d8dad3b1e9f85311bc4108a29
Robbie Mcilwaine <robbiemcilwaine99@gmail.com>
2024-06-18 14:17:58 +0100

    wrote tests for sum

---
+++ calculator.test.js
@@ -0,0 +1,61 @@
+const {
+  sum,
+  subtract,
+  multiply,
+  divide,
+  modulus,
+  even,
+  odd,
+} = require("./calculator");
+
+describe('sum', () => { 
+  
+  test('can add two small positive numbers', () => {
+    expected = 5;
+    actual = sum(2, 3);
+    expect(actual).toBe(expected);
+  });
+
+  test.skip('can add two large positive numbers', () => { 
+    expected = 150;
+    actual = sum(100, 50);
+    expect(actual).toBe(expected);
+  });
+
+  test.skip('can add two negative numbers', () => {
+    expected = -5;
+    actual = sum(-2, -3);
+    expect(actual).toBe(expected);
+  });
+
+  test.skip('can add zero', () => {
+    expected = 8;
+    actual = sum(8, 0);
+    expect(actual).toBe(expected);
+  });
+
+});
+
+describe('subtract', () => {
+
+});
+
+describe('multiply', () => {
+
+});
+
+describe('divide', () => {
+
+});
+
+describe('modulus', () => {
+
+});
+
+describe('even', () => {
+
+});
+
+describe('odd', () => {
+
+});
